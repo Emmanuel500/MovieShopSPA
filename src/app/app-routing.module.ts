498c5d42844ad1c0c0d6252a8037152f2bd56c93
Emmanuel500 <32080181+Emmanuel500@users.noreply.github.com>
2022-03-19 21:09:34 -0400

    Set up the start of Movie Shop SPA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+
+  // load the admin module(along with its components) lazily
+  // http://localhost:4200/admin
+  {
+    path: "admin", loadChildren: () => import("./admin/admin.module").then(mod => mod.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
